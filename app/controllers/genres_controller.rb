be125d5fd460b84fcf7bf8085d062b56ac900a6f
metildachee <metildachee@gmail.com>
2020-08-03 00:07:19 +0800

    update manga genres, create, edit genre ok

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,11 +4,38 @@ class GenresController < ApplicationController
   end
 
   def show
+    @genre = Genre.find(params[:id])
   end
 
   def new
+    @genre = Genre.new
   end
+
+  def create
+    if Genre.create(genre_param).save
+      redirect_to genres_path
+    else 
+      render :new
+    end 
+  end
+
+  def edit 
+    @genre = Genre.find(params[:id])
+  end 
+
+  def update 
+    if Genre.find(params[:id]).update(genre_param)
+      redirect_to genres_path
+    else 
+      render :edit
+    end 
+  end 
 
   def destroy
   end
+
+  private 
+  def genre_param
+    params.require(:genre).permit(:name, :desc)
+  end 
 end
--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -9,20 +9,23 @@ class MangasController < ApplicationController
 
   def edit
     @manga = Manga.find(params[:id])
+    @genres = Genre.all
     @artists = Artist.all
   end
 
-  def update
+  def update # must be a better way to write this function..
     @manga = Manga.find(params[:id])
 
     if params[:artist] == @manga.artist.id && @manga.update(mangas_params)
       # no changes to the artist ID
+      update_genre(@manga, params[:genres]).save
       redirect_to mangas_path
     elsif params[:artist] != @manga.artist.id
       # changed to another artist
       @manga.destroy # we destroy this instance, also removing all references
-      if Artist.find(params[:artist]).mangas.build(mangas_params).save 
-        # build a new one with the new artist ID
+      @manga = Artist.find(params[:artist]).mangas.build(mangas_params) # build a new one with the new artist ID
+      if update_genre(@manga, params[:genres]).save
+        # update_genre(@manga)
         redirect_to mangas_path
       else 
         render :edit
@@ -40,12 +43,8 @@ class MangasController < ApplicationController
 
   def create
     @manga = Artist.find(params[:artist]).mangas.build(mangas_params)
-    
-    params[:genres].each do |genre_id|
-      @manga.genre << Genre.find(genre_id)
-    end 
 
-    if @manga.save
+    if update_genre(@manga, params[:genres]).save
         redirect_to mangas_path
     else 
         render :new 
@@ -63,4 +62,16 @@ class MangasController < ApplicationController
   def mangas_params
     params.require(:manga).permit(:title, :desc, :img_path, :artist, :genre_id)
   end 
+
+  # @params manga => an manga AR
+  # @params genres_arr => an array of genre.id
+  # @returns the updated manga
+  def update_genre(manga, genres_arr)
+    manga.genre.clear
+    genres_arr.each do |genre_id|
+      manga.genre << Genre.find(genre_id)
+    end 
+    return manga
+  end
+  
 end
